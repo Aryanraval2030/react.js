61efbad9f8f9b5b5f35d150782dd48ba448bbb08
aryan raval <aryanraval1432@gmail.com.>
2026-02-10 12:10:28 +0530

    tailwind-first

--- day-4/src/components/section-1/Section1.jsx
+++ day-4/src/components/section-1/Section1.jsx
@@ -6,16 +6,16 @@ import { Bird } from 'lucide-react';
 function Section1() {
     return (
         <div>
-            <div className="w-screen mt-10 sm:flex tracking-wide overflow-x-hidden">
+            <div className="w-screen mt-10 sm:flex tracking-wide overflow-x-hidden text-center">
 
-                <div className="w-screen sm:w-full sm:pl-10 pl-2">
+                <div className="w-screen sm:w-full sm:pl-10 pl-2 ">
                     <h1 className="text-5xl font-semibold text-purple-500 mb-5">join our club</h1>
                     <h1 className="text-3xl font-semibold">yoga for your <br /> healthy life</h1>
                     <p className="mt-5">join our club and discover this true benefit of yoga enhance <br />  flexibility reduce stress, and embrace a healthir, balanced lifestyle with guided sessions made for everyone.</p>
                     <button className="mt-5 borde rounded-3xl px-8 py-2 bg-purple-700 text-white">book now</button>
 
-                    <div className='flex mt-5 justify-between w-36'>
-                        <span className='rounded-3xl bg-purple-700 p-2'><Instagram /></span>
+                    <div className='flex mt-5 justify-center gap-3'>
+                        <span className='rounded-3xl bg-purple-700  p-2'><Instagram  className='text-white' /></span>
                         <span className='rounded-3xl bg-purple-700 p-2'><Facebook /></span>
                         <span className='rounded-3xl bg-purple-700 p-2'><Bird /></span>
                     </div>
