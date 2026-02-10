0c68385a36f7240f955c7f01d4b4359464bf33cf
aryan raval <aryanraval1432@gmail.com.>
2026-02-10 12:17:12 +0530

    day-4

--- day-4/src/components/header/Header.jsx
+++ day-4/src/components/header/Header.jsx
@@ -14,9 +14,48 @@ function Header() {
                         <li><a href="#portfolio">portfolio</a></li>
                         <li><a href="#contact-us">contact us</a></li>
                     </ul>
+
+
+                    
                     <div className='sm:hidden'>
-                        <Logs />
-                    </div> 
+                        <details className="relative md:hidden">
+                            <summary className="list-none cursor-pointer">
+                                <svg
+                                    className="w-7 h-7 text-gray-700"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M4 6h16M4 12h16M4 18h16"
+                                    />
+                                </svg>
+                            </summary>
+
+                            <div className="absolute right-0 mt-4 w-56 rounded-xl bg-white shadow-lg overflow-hidden">
+                                <a href="#" className="block px-6 py-3 text-gray-600 hover:bg-indigo-50 hover:text-indigo-600">
+                                    Home
+                                </a>
+                                <a href="#" className="block px-6 py-3 text-gray-600 hover:bg-indigo-50 hover:text-indigo-600">
+                                    service
+                                </a>
+                                <a href="#" className="block px-6 py-3 text-gray-600 hover:bg-indigo-50 hover:text-indigo-600">
+                                    portfolio
+                                </a>
+                                <a href="#" className="block px-6 py-3 text-gray-600 hover:bg-indigo-50 hover:text-indigo-600">
+                                    Contact us
+                                </a>
+                                <div className="p-4">
+                                    <button className="w-full rounded-full bg-indigo-600 py-2 text-white hover:bg-indigo-700 transition">
+                                        Get Started
+                                    </button>
+                                </div>
+                            </div>
+                        </details>
+                        </div>
                 </nav>
             </header>
 
