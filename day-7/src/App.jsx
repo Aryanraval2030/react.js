7b784161e8a5841a21c37c23d81b460d131fc389
aryan raval <aryanraval1432@gmail.com.>
2026-02-19 12:37:39 +0530

    data-store-in-local-storage

---
+++ day-7/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <button >
+        Click
+      </button>
+
+    </>
+  )
+}
+
+export default App
---
+++ day-7/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ], theme: {
+    colors: {
+      primary: "#0F172A",
+      secondary: "#F97316",
+      white: "#ffffff",
+    },
+  },
+  plugins: [],
+}
+
